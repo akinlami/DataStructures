9edb8d37040be70e9916930502adfbf69fa3ce8e
bi54jf <bi54jf@FOT-CELLB15>
2024-10-03 12:32:19 +0100

    Changes to TVSHOWS CLASS

--- DataStructures/TVShow.cs
+++ DataStructures/TVShow.cs
@@ -8,7 +8,22 @@ namespace DataStructures
 {
     internal class TVShow
     {
+        public string title = "Untitled";
+        public int releaseYear;
+        public double price;
+        public MovieRating rating;
 
+        public TVShow(string title, int releaseYear, double price, MovieRating rating)
+        {
+            this.title = title;
+            this.releaseYear = releaseYear;
+            this.price = price;
+            this.rating = rating;
+        }
 
+        public override string ToString()
+        { 
+            return title + "(" + releaseYear +") Rated" + rating + "...£" + price.ToString("0.00");
+        }
     }
 }
